360b980464d4d947fb61226472cd36b4c4f1987c
Adrian Farnell <adrianjfarnell@gmail.com>
2016-08-11 12:25:01 +0200

    initial commit

---
+++ unitTest-cryptNote/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace unitTest_cryptNote
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
